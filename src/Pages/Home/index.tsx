cc60de8260747f19786c2e78dcdc09bba9689be1
CaioAlmeida09 <Caio09092001@gmail.com>
2024-01-01 20:41:45 -0300

    atualizando select e options

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,36 +1,86 @@
 import { db } from "../../services/firebaseconection";
-import { collection, addDoc } from "firebase/firestore";
-import React, { useState } from "react";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import React, { useState, useEffect } from "react";
 import { Header } from "../../components/header";
+
+interface TamanhoProps {
+  "P-Cinza-Masc": number;
+  "P-Cinza-Fem": number;
+  "P-Azul-Masc": number;
+  "P-Azul-Fem": number;
+  "M-Cinza-Masc": number;
+  "M-Cinza-Fem": number;
+  "M-Azul-Masc": number;
+  "M-Azul-Fem": number;
+  "GG-Cinza-Masc": number;
+  "GG-Cinza-Fem": number;
+  "GG-Azul-Masc": number;
+  "GG-Azul-Fem": number;
+  "G-Cinza-Masc": number;
+  "G-Cinza-Fem": number;
+  "G-Azul-Masc": number;
+  "G-Azul-Fem": number;
+  "XGG-Azul-Masc": number;
+  "XGG-Cinza-Masc": number;
+}
 
 function Home() {
   const [corCamisa, setCorCamisa] = useState({
     corCamisa: "bg-black",
   });
+  const [Tamanhos, setTamanhos] = useState<TamanhoProps[]>([]);
+
+  useEffect(() => {
+    async function getPedidos() {
+      const pedidosRef = collection(db, "tamanhos");
+      const data = await getDocs(pedidosRef);
+      const pedidoRef = data.docs.map((doc) => ({
+        ...doc.data(),
+      })) as TamanhoProps[];
+
+      setTamanhos(pedidoRef);
+      console.log(pedidoRef);
+    }
+
+    getPedidos();
+  }, []);
 
   const HandleSubmit = async () => {
-    const corSelecionada = document.getElementById(
-      "corCamisa"
-    ) as HTMLSelectElement;
     const tamanhoSelecionado = document.getElementById(
-      "tamanhoCamisa"
+      "tamanhos"
     ) as HTMLSelectElement;
-    const sexoSelecionado = document.getElementById(
-      "sexo"
-    ) as HTMLSelectElement;
+
     const quantidadeSelecionada = document.getElementById(
       "quantidade"
     ) as HTMLSelectElement;
     const idDigitado = document.getElementById("id") as HTMLInputElement;
 
     const camisaData = {
-      corCamisa: corSelecionada.value,
-      tamanhoCamisa: tamanhoSelecionado.value,
-      sexo: sexoSelecionado.value,
+      tamanhos: tamanhoSelecionado.value,
       quantidade: Number(quantidadeSelecionada.value),
       id: Number(idDigitado.value),
     };
 
+    if (
+      camisaData.tamanhos === "P-Azul-Fem" ||
+      camisaData.tamanhos === "P-Azul-Masc" ||
+      camisaData.tamanhos === "M-Azul-Masc" ||
+      camisaData.tamanhos === "M-Azul-Fem" ||
+      camisaData.tamanhos === "G-Azul-Fem" ||
+      camisaData.tamanhos === "G-Azul-Masc" ||
+      camisaData.tamanhos === "GG-Azul-Masc" ||
+      camisaData.tamanhos === "GG-Azul-Fem" ||
+      camisaData.tamanhos === "XGG-Azul-Masc"
+    ) {
+      console.log("Tamanho selecionado:", tamanhoSelecionado.value);
+      console.log("Cor da camisa após setCorCamisa:", corCamisa);
+    }
     console.log("Dados a serem enviados:", camisaData);
 
     try {
@@ -40,6 +90,18 @@ function Home() {
     } catch (error) {
       console.error("Erro ao enviar dados:", error);
     }
+
+    const documentId = "PZiKQTer4Eib9atJKvFv";
+    const documentoRef = doc(db, "tamanhos", documentId);
+    updateDoc(documentoRef, {
+      "G-Azul-Fem": 1,
+    })
+      .then(() => {
+        console.log("Documento atualizado com sucesso!");
+      })
+      .catch((error) => {
+        console.error("Erro ao atualizar documento: ", error);
+      });
   };
 
   const handleCorCamisaChange = (event) => {
@@ -60,38 +122,33 @@ function Home() {
               : ""
           }`}
         >
-          <section className="flex gap-5">
-            <label htmlFor="corCamisa">Cor da Camisa:</label>
-            <select
-              id="corCamisa"
-              name="corCamisa"
-              onChange={handleCorCamisaChange}
-              value={corCamisa.corCamisa}
-            >
-              <option value="cinzaChumbo">Cinza Chumbo</option>
-              <option value="azulRoyal">Azul Royal</option>
+          <section className="flex flex-col gap-5">
+            <label> Selecione o Tamanho e Cor da Camisa</label>
+            <select id="tamanhos">
+              <option value="P-Cinza-Masc">P - Cinza - Masculina</option>
+              <option value="P-Cinza-Fem">P - Cinza - Feminina</option>
+              <option value="P-Azul-Masc">P - Azul - Masculina</option>
+              <option value="P-Azul-Fem">P - Azul - Feminina</option>
+              <option value="M-Cinza-Fem">M - Cinza - Feminina</option>
+              <option value="M-Cinza-Masc">M - Cinza - Masculino</option>
+              <option value="M-Azul-Fem">M - Azul - Feminina</option>
+              <option value="M-Azul-Masc">M - Azul - Masculina</option>
+              <option value="G-Cinza-Fem">G - Cinza - Feminina</option>
+              <option value="G-Cinza-Masc">G - Cinza - Masculina</option>
+              <option value="G-Azul-Fem">G - Azul - Feminina</option>
+              <option value="G-Azul-Masc">G - Azul - Masculino</option>
+              <option value="GG-Cinza-Fem">GG - Cinza - Feminina</option>
+              <option value="GG-Cinza-Masc">GG - Cinza - Masculino</option>
+              <option value="GG-Azul-Fem">GG - Azul - Feminina</option>
+              <option value="GG-Azul-Masc">GG - Azul - Masculino</option>
+              <option value="XGG-Azul-Masc">XGG - Cinza - Masculino</option>
+              <option value="XGG-Cinza-Masc">XGG - Azul - Masculino</option>
             </select>
           </section>
-          <section className="flex gap-5">
-            <label htmlFor="tamanhoCamisa">Tamanho da camisa</label>
-            <select id="tamanhoCamisa" name="tamanhoCamisa">
-              <option value="P">P</option>
-              <option value="M">M</option>
-              <option value="G">G</option>
-              <option value="GG">GG</option>
-              <option value="XGG">XGG</option>
-            </select>
-          </section>
-          <section className="flex gap-5">
-            <label htmlFor="sexo">Sexo</label>
-            <select id="sexo" name="sexo">
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
-            </select>
-          </section>
+
           <section className="flex gap-5">
             <label htmlFor="quantidade">Quantidade:</label>
-            <select id="quantidade" name="quantidade">
+            <select id="quantidade" name="quantidade" className="h-8">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -99,7 +156,7 @@ function Home() {
           </section>
           <section className="flex gap-5">
             <label htmlFor="id">ID:</label>
-            <input id="id" className="w-full h-6 p-1" />
+            <input id="id" className="w-full h-8 p-1" />
           </section>
           <button
             type="button"
