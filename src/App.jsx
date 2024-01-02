540cc8a5628994bc092a33079dc4359de84a3fc1
CaioAlmeida09 <Caio09092001@gmail.com>
2024-01-02 05:45:27 -0300

    ajustando App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Search } from "./Pages/search";
 import { View } from "./Pages/view";
 import { Estoque } from "./Pages/estoque";
-import { Home } from "./Pages/home";
+import { Home } from "./Pages/home/index";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home} />
+        <Route path="/" element={<Home />} />
         <Route path="/Search" element={<Search />} />
         <Route path="/View" element={<View />} />
         <Route path="/Estoque" element={<Estoque />} />
